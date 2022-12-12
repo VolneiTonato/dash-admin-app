1263c89357c194acde8ff4344eb63f28650f84d8
Volnei Tonato <volneitonato@gmail.com>
2022-12-12 01:57:18 -0300

    update: add commands start app

--- commands.sh
+++ commands.sh
@@ -1,7 +1,18 @@
+python3 -m venv .venv
+
 . .venv/bin/activate
 
-python pyapp/app.py
+pip install -r requirements.txt
 
 python pyapp/app.py --command=drop-db
 python pyapp/app.py --command=create-db
 python pyapp/app.py --command=populate-db
+
+python pyapp/app.py
+
+
+localhost:8050
+
+username: administrator
+pass: 123
+
