9ed5f432adfb31a8d46a0a9d9cd5ba9581371a43
Volnei Tonato <volneitonato@gmail.com>
2022-12-14 09:37:28 -0300

    upadate

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
 docker build . -t tonato/dash-admin-app:latest
-
-# docker push tonato/dash-admin-app:latest
--- start_app.sh
+++ start_app.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 . .venv/bin/activate && \
-gunicorn main:'create_app()'
-# python -m http.server 8050
-    # python main.py --command=drop-db && \
-    # python main.py --command=create-db && \
-    # python main.py --command=populate-db && \
-    # python main.py
+python main.py --command=drop-db && \
+python main.py --command=create-db && \
+python main.py --command=populate-db
